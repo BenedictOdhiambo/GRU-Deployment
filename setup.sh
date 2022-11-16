5b7ead0e780e4a86c88df55e50a7d6f5ff8486a7
Benedict Odhiambo <benedictodhiambo751@gmail.com>
2022-11-16 12:45:47 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
-mkdir -p ~/.streamlit/
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-\n\
-" > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/ 
+    echo "\ [server]\n\
+    headless = true\n\
+    port = $PORT\n\
+    enableCORS = false\n\
+    \n\" > ~/.streamlit/config.toml
