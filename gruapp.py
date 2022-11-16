38ca38360c162accb9cd583f97a991e674c702cb
Benedict Odhiambo <benedictodhiambo751@gmail.com>
2022-11-16 22:14:42 +0300

    Update gruapp.py

--- gruapp.py
+++ gruapp.py
@@ -37,7 +37,7 @@ def main():
     st.header('Enter the timestamp:')
     timestamp = st.number_input('timestamp:', min_value=1, max_value=1000, value=1)
     output=""
-    st.button("Predict Passengers"):
+    st.button("Predict Passengers")
     output = predict_Airpassengers(timestamp)
     st.success('The output is ;'.format(output))
     main()
